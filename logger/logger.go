8b4977b01fbfdf2f9aa371c517d07798b0655db1
Cre4T3Tiv3 <jlmoses@outlook.com>
2023-11-02 17:05:29 -0600

    enhance(database): improve interactions, error handling, and logging.

--- logger/logger.go
+++ logger/logger.go
@@ -15,7 +15,7 @@ var (
 
 // InitLog initializes the loggers.
 func InitLog() {
-	file, err := os.OpenFile("info.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	file, err := os.OpenFile("logs/app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal("Failed to open log file:", err)
 	}
