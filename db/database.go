558dcb30ea60f1633793e43cef22cc1b7fef1df6
Cre4T3Tiv3 <jlmoses@outlook.com>
2023-11-02 10:12:37 -0600

    enhance(database): improve interactions, error handling, and logging.

--- db/database.go
+++ db/database.go
@@ -3,11 +3,7 @@ package db
 import (
 	"fmt"
 	"log"
-<<<<<<< HEAD
-
-=======
 	"os"
->>>>>>> main
 	"weather-history-api/configs"
 	"weather-history-api/models"
 
@@ -15,13 +11,6 @@ import (
 	"gorm.io/gorm"
 )
 
-<<<<<<< HEAD
-var DBConn *gorm.DB
-
-// SetDBConnection sets the global DB connection with the provided database instance.
-func SetDBConnection(database *gorm.DB) {
-	DBConn = database
-=======
 var DB *gorm.DB
 var logger *log.Logger
 
@@ -33,7 +22,6 @@ func init() {
 	}
 	// Create a logger instance to write logs to the file.
 	logger = log.New(logFile, "", log.LstdFlags|log.Lshortfile)
->>>>>>> main
 }
 
 // GetDBConnection retrieves the global DB connection.
@@ -49,16 +37,10 @@ func InitDB(config configs.Config) error {
 	if err != nil {
 		return fmt.Errorf("error opening database: %v", err)
 	}
-<<<<<<< HEAD
-
-	log.Println("Database successfully connected!")
-	return nil
-=======
 	log.Println("Database successfully connected!")
 	logger.Println("Database successfully connected!")
 	// Auto migration for Weather model.
 	DB.AutoMigrate(&models.Weather{})
->>>>>>> main
 }
 
 func CloseDB() {
